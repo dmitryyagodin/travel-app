35693cd4b437823ed8f5fe09e97c8dc05af0158b
Veronika <v.vasilchykava@gmail.com>
2021-03-08 19:00:19 +0300

    refactor: modified countries data structure

---
+++ client/src/dummy-data/index.tsx
@@ -0,0 +1,388 @@
+import { CountryItem } from '../interfaces/interfaces';
+import Cuba from './countries/cuba.jpg';
+import Albania from './countries/albania.jpg';
+import Peru from './countries/peru.jpg';
+import Canada from './countries/canada.jpg';
+import Iceland from './countries/iceland.jpg';
+import Portugal from './countries/portugal.jpg';
+import Jordan from './countries/jordan.jpg';
+import CostaRica from './countries/сostarica.jpg';
+
+export const Countries: Array<CountryItem> = [
+  {
+    id: 1,
+    picture: Cuba,
+    countryName: 'The Republic of Cuba',
+    capitalName: 'Havana',
+    lang: 'Spanish',
+    description: '',
+    video: '',
+    sights: [
+      {
+        id: 1,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 2,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 3,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 4,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 5,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 6,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+    ]
+  },
+  {
+    id: 2,
+    picture: Albania,
+    countryName: 'The Republic of Albania',
+    capitalName: 'Tirana',
+    lang: 'Albanian',
+    description: '',
+    video: '',
+    sights: [
+      {
+        id: 1,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 2,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 3,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 4,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 5,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 6,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+    ]
+  },
+  {
+    id: 3,
+    picture: Peru,
+    countryName: 'The Republic of Peru',
+    capitalName: 'Lima',
+    lang: 'Spanish',
+    description: '',
+    video: '',
+    sights: [
+      {
+        id: 1,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 2,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 3,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 4,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 5,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 6,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+    ]
+  },
+  {
+    id: 4,
+    picture: Canada,
+    countryName: 'Canada',
+    capitalName: 'Ottawa',
+    lang: 'English, French',
+    description: '',
+    video: '',
+    sights: [
+      {
+        id: 1,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 2,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 3,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 4,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 5,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 6,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+    ]
+  },
+  {
+    id: 5,
+    picture: Iceland,
+    countryName: 'Iceland',
+    capitalName: 'Reykjavik',
+    lang: 'Icelandic',
+    description: '',
+    video: '',
+    sights: [
+      {
+        id: 1,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 2,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 3,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 4,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 5,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 6,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+    ]
+  },
+  {
+    id: 6,
+    picture: Portugal,
+    countryName: 'The Portuguese Republic',
+    capitalName: 'Lisbon',
+    lang: 'Portuguese',
+    description: '',
+    video: '',
+    sights: [
+      {
+        id: 1,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 2,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 3,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 4,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 5,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 6,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+    ]
+  },
+  {
+    id: 7,
+    picture: Jordan,
+    countryName: 'The Hashemite Kingdom of Jordan',
+    capitalName: 'Amman',
+    lang: 'Arabic',
+    description: '',
+    video: '',
+    sights: [
+      {
+        id: 1,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 2,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 3,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 4,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 5,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 6,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+    ]
+  },
+  {
+    id: 8,
+    picture: CostaRica,
+    countryName: 'The Republic of Costa Rica',
+    capitalName: 'San Jose',
+    lang: 'spanish',
+    description: '',
+    video: '',
+    sights: [
+      {
+        id: 1,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 2,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 3,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 4,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 5,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+      {
+        id: 6,
+        picture: '',
+        sightName: '',
+        description: ''
+      },
+    ]
+  }
+];
--- client/src/interfaces/interfaces.tsx
+++ client/src/interfaces/interfaces.tsx
@@ -1,12 +1,16 @@
+export interface SightItem {
+  id: number;
+  picture: any;
+  sightName: string;
+  description: string;
+}
 export interface CountryItem {
   id: number;
+  picture: any;
   countryName: string;
   capitalName: string;
   lang: string;
-}
-
-export interface CardItem {
-  countryName: string;
-  sight: string;
   description: string;
+  video: string;
+  sights: Array<SightItem>
 }
