1a4e083a9979d6134601d481513fc6f07ad45640
Veronika <v.vasilchykava@gmail.com>
2021-03-08 19:16:20 +0300

    feat: add videos and description of countriess

--- client/src/dummy-data/index.tsx
+++ client/src/dummy-data/index.tsx
@@ -15,8 +15,8 @@ export const Countries: Array<CountryItem> = [
     countryName: 'The Republic of Cuba',
     capitalName: 'Havana',
     lang: 'Spanish',
-    description: '',
-    video: '',
+    description: 'This is a country comprising the island of Cuba, as well as Isla de la Juventud and several minor archipelagos. Cuba is located in the northern Caribbean where the Caribbean Sea, Gulf of Mexico and Atlantic Ocean meet. It is east of the Yucatán Peninsula (Mexico), south of both the U.S. state of Florida and the Bahamas, west of Hispaniola, and north of both Jamaica and the Cayman Islands. Havana is the largest city and capital; other major cities include Santiago de Cuba and Camagüey. The official area of the Republic of Cuba is 109,884 km2 (42,426 sq mi) (without the territorial waters). The main island of Cuba is the largest island in Cuba and in the Caribbean, with an area of 104,556 km2 (40,369 sq mi). Cuba is the second-most populous country in the Caribbean after Haiti, with over 11 million inhabitants.',
+    video: 'https://www.youtube.com/watch?v=exfjK7rvLUY',
     sights: [
       {
         id: 1,
@@ -62,8 +62,8 @@ export const Countries: Array<CountryItem> = [
     countryName: 'The Republic of Albania',
     capitalName: 'Tirana',
     lang: 'Albanian',
-    description: '',
-    video: '',
+    description: 'is a country in Southeastern Europe. It is located on the Adriatic and Ionian Sea within the Mediterranean Sea, and shares land borders with Montenegro to the northwest, Kosovo[c] to the northeast, North Macedonia to the east, Greece to the south; and maritime borders with Greece, Montenegro and Italy to the west. Tirana is its capital and largest city, followed by Durrës, Vlorë and Shkodër.',
+    video: 'https://www.youtube.com/watch?v=TcggfMkWgpg',
     sights: [
       {
         id: 1,
@@ -109,8 +109,8 @@ export const Countries: Array<CountryItem> = [
     countryName: 'The Republic of Peru',
     capitalName: 'Lima',
     lang: 'Spanish',
-    description: '',
-    video: '',
+    description: 'This is a country in western South America. It is bordered in the north by Ecuador and Colombia, in the east by Brazil, in the southeast by Bolivia, in the south by Chile, and in the south and west by the Pacific Ocean. Peru is a megadiverse country with habitats ranging from the arid plains of the Pacific coastal region in the west to the peaks of the Andes mountains extending from the north to the southeast of the country to the tropical Amazon Basin rainforest in the east with the Amazon river.[9] At 1.28 million km2 (0.5 million mi2), Peru is the 19th largest country in the world, and the third largest in South America.',
+    video: 'https://www.youtube.com/watch?v=wvdc8S1WoE8',
     sights: [
       {
         id: 1,
@@ -156,8 +156,8 @@ export const Countries: Array<CountryItem> = [
     countryName: 'Canada',
     capitalName: 'Ottawa',
     lang: 'English, French',
-    description: '',
-    video: '',
+    description: 'This is a country in the northern part of North America. Its ten provinces and three territories extend from the Atlantic to the Pacific and northward into the Arctic Ocean, covering 9.98 million square kilometres (3.85 million square miles), making it the worlds second-largest country by total area. Its southern and western border with the United States, stretching 8,891 kilometres (5,525 mi), is the worlds longest bi-national land border. Canadas capital is Ottawa, and its three largest metropolitan areas are Toronto, Montreal, and Vancouver.',
+    video: 'https://www.youtube.com/watch?v=FqrcRmGPWQc',
     sights: [
       {
         id: 1,
@@ -203,8 +203,8 @@ export const Countries: Array<CountryItem> = [
     countryName: 'Iceland',
     capitalName: 'Reykjavik',
     lang: 'Icelandic',
-    description: '',
-    video: '',
+    description: 'Iceland is volcanically and geologically active. The interior consists of a plateau characterised by sand and lava fields, mountains, and glaciers, and many glacial rivers flow to the sea through the lowlands. Iceland is warmed by the Gulf Stream and has a temperate climate, despite a high latitude just outside the Arctic Circle. Its high latitude and marine influence keep summers chilly, with most of its islands having a polar climate.',
+    video: 'https://www.youtube.com/watch?v=wwVoA_s60JQ',
     sights: [
       {
         id: 1,
@@ -250,8 +250,8 @@ export const Countries: Array<CountryItem> = [
     countryName: 'The Portuguese Republic',
     capitalName: 'Lisbon',
     lang: 'Portuguese',
-    description: '',
-    video: '',
+    description: 'is a country located on the Iberian Peninsula, in southwestern Europe. It is the westernmost sovereign state in mainland Europe, being bordered to the west and south by the Atlantic Ocean and to the north and east by Spain. Its territory also includes the Atlantic archipelagos of the Azores and Madeira, both autonomous regions with their own regional governments. The official and national language is Portuguese.',
+    video: 'https://www.youtube.com/watch?v=KCgtPL1D6vM',
     sights: [
       {
         id: 1,
@@ -297,8 +297,8 @@ export const Countries: Array<CountryItem> = [
     countryName: 'The Hashemite Kingdom of Jordan',
     capitalName: 'Amman',
     lang: 'Arabic',
-    description: '',
-    video: '',
+    description: 'is an Arab country in the Levant region of Western Asia, on the East Bank of the Jordan River. Jordan is bordered by Saudi Arabia, Iraq, Syria, Israel and Palestine (West Bank). The Dead Sea is located along its western borders and the country has a 26-kilometre (16 mi) coastline on the Red Sea in its extreme south-west.[8] Jordan is strategically located at the crossroads of Asia, Africa and Europe.[9] The capital, Amman, is Jordans most populous city as well as the countrys economic, political and cultural centre.',
+    video: 'https://www.youtube.com/watch?v=npjIIfN1f-M',
     sights: [
       {
         id: 1,
@@ -344,8 +344,8 @@ export const Countries: Array<CountryItem> = [
     countryName: 'The Republic of Costa Rica',
     capitalName: 'San Jose',
     lang: 'spanish',
-    description: '',
-    video: '',
+    description: ' is a country in Central America, bordered by Nicaragua to the north, the Caribbean Sea to the northeast, Panama to the southeast, the Pacific Ocean to the southwest, and Ecuador to the south of Cocos Island. It has a population of around 5 million[5][6] in a land area of 51,060 square kilometers (19,714 square miles). An estimated 333,980 people live in the capital and largest city, San José, with around 2 million people in the surrounding metropolitan area.',
+    video: 'https://www.youtube.com/watch?v=Ci--UvWrsbk',
     sights: [
       {
         id: 1,
