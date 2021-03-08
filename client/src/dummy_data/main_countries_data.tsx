058240171434f70fcf459afc28ef1f4afd052b34
Veronika <v.vasilchykava@gmail.com>
2021-03-08 18:25:04 +0300

    feat: add pictures and data for main page

---
+++ client/src/dummy_data/main_countries_data.tsx
@@ -0,0 +1,52 @@
+import { CountryItem } from '../interfaces/interfaces';
+
+export const countries: Array<CountryItem> = [
+  {
+    id: 1,
+    countryName: 'The Republic of Cuba',
+    capitalName: 'Havana',
+    lang: 'Spanish',
+  },
+  {
+    id: 2,
+    countryName: 'The Republic of Albania',
+    capitalName: 'Tirana',
+    lang: 'Albanian',
+  },
+  {
+    id: 3,
+    countryName: 'The Republic of Peru',
+    capitalName: 'Lima',
+    lang: 'Spanish',
+  },
+  {
+    id: 4,
+    countryName: 'Canada',
+    capitalName: 'Ottawa',
+    lang: 'English, French',
+  },
+  {
+    id: 5,
+    countryName: 'Iceland',
+    capitalName: 'Reykjavik',
+    lang: 'Icelandic',
+  },
+  {
+    id: 6,
+    countryName: 'The Portuguese Republic',
+    capitalName: 'Lisbon',
+    lang: 'Portuguese',
+  },
+  {
+    id: 7,
+    countryName: 'The Hashemite Kingdom of Jordan',
+    capitalName: 'Amman',
+    lang: 'Arabic',
+  },
+  {
+    id: 8,
+    countryName: 'The Republic of Costa Rica',
+    capitalName: 'San Jose',
+    lang: 'spanish',
+  }
+];
---
+++ client/src/interfaces/interfaces.tsx
@@ -0,0 +1,12 @@
+export interface CountryItem {
+  id: number;
+  countryName: string;
+  capitalName: string;
+  lang: string;
+}
+
+export interface CardItem {
+  countryName: string;
+  sight: string;
+  description: string;
+}
