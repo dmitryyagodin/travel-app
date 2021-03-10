dd63a7d6abcfeb07aa5f6e031d2e1212b720ae0b
vio7enc3 <nicksakerin96@gmail.com>
2021-03-10 11:10:55 +0500

    feat: add sign in and signout buttons

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Context } from '../../context/MainContext'
+import { SignIn } from '../SignIn'
+import { SignOut } from '../SignOut'
 import './Header.scss'
 
 export const Header: React.FC = () => {
+  const { user } = React.useContext(Context)
   return (
-    <header className="header">
-      Travel App
+    <header>
+      <div className="header">
+        <div className="header__logo">
+          Travel APP
+        </div>
+        <div className="header__sign">
+          {user ? <SignOut /> : <SignIn />}
+        </div>
+      </div>
     </header>
   )
 }
