716fbab7f81cb331fdb06f25dce3dfb739647b8d
vio7enc3 <nicksakerin96@gmail.com>
2021-03-10 11:12:06 +0500

    commented homeslides

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Context } from '../../context/MainContext';
 import { HomeSlides } from '../HomeSlides';
 import './Home.scss';
 
 export const Home:React.FC = () => {
+  const { user } = React.useContext(Context)
   return (
     <div className="container-xl">
-      <HomeSlides />
+      {/* <HomeSlides /> */}
+      {user && <h1>Hello, {user.displayName}</h1>}
     </div>
   )
 }
