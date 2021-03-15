f52618f1564fae5649974117481a6b8bfff414b0
Dmitry Yagodin <dmitry.yagodin@gmail.com>
2021-03-15 10:04:24 +0200

    feat: add footer elements and styling

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Footer.scss';
 import { FooterItem } from '../../interfaces/interfaces';
+import rsLogo from '../../assets/icons/rs_school_js.svg';
+import githubIcon from '../../assets/icons/githubIcon.svg'
 
 export const Footer: React.FC<FooterItem> = (props) => {
   const langItem: string = props.langValue;
@@ -12,7 +14,19 @@ export const Footer: React.FC<FooterItem> = (props) => {
 
   return (
     <footer className="footer">
-      Footer
+      <a href="https://rs.school/react/">
+        <img src={rsLogo} alt="" />
+      </a>
+      <span>2021</span>
+      <div className="dropup">
+        <button className="dropbtn"><img src={githubIcon} alt=""/></button>
+        <div className="dropup-content">
+          <a href="https://github.com/vio7enc3">vio7enc3</a>
+          <a href="https://github.com/VeronikaVasilchikova">VeronikaVasilchikova</a>
+          <a href="https://github.com/GeorgeTuz">GeorgeTuz</a>
+          <a href="https://github.com/dmitryyagodin">dmitryyagodin</a>
+        </div>
+      </div>
     </footer>
   )
 }
